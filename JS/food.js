c7ca238fe3c7f159f9ac8ab7b9873f48a7f7ca08
CNSAKIB <c.nazmuzsakib@gmail.com>
2021-08-28 23:03:49 +0600

    try catch added

--- JS/food.js
+++ JS/food.js
@@ -4,7 +4,6 @@ const displaySearchedItem = () => {
 
     const inputFeild = document.getElementById('search-feild');
     const inputValue = inputFeild.value;
-    // console.log(inputValue);
     inputFeild.value = '';
 
     if (inputValue == '') {
@@ -16,9 +15,15 @@ const displaySearchedItem = () => {
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySearchResult(data.meals));
+            .then(data => displaySearchResult(data.meals))
+            .catch(error => displayError(error));
+
         document.getElementById('spinner-display').style.display = 'block';
 
+    }
+
+    const displayError = error => {
+        document.getElementById('no-food').style.display = 'block';
     }
 }
 
@@ -27,17 +32,12 @@ const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     document.getElementById('spinner-display').style.display = 'none';
-    if (meals.length == 0) {
-        console.log(meals);
-        document.getElementById('no-food').style.display = 'block';
-    }
-    else {
-        document.getElementById('no-food').style.display = 'none';
-        for (const meal of meals) {
-            console.log(meal);
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+    document.getElementById('no-food').style.display = 'none';
+    for (const meal of meals) {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
                  <div class="card">
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -45,8 +45,7 @@ const displaySearchResult = meals => {
                         <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
                  </div>
             </div>`
-            searchResult.appendChild(div);
-        }
+        searchResult.appendChild(div);
     }
 
 
